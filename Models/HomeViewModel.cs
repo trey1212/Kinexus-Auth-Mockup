f3863080e42a030d472a178443a6e346477a1c23
Trey Tarrayo <rtarrayo2@my.bcit.ca>
2026-05-14 21:43:16 -0700

    UI Changes

--- Models/HomeViewModel.cs
+++ Models/HomeViewModel.cs
@@ -3,4 +3,5 @@ namespace KinexusMockup.Models;
 public class HomeViewModel
 {
     public string WelcomeMessage { get; set; }
+    public string SignetMessage { get; set; }
 }
--- Services/HomeContentService.cs
+++ Services/HomeContentService.cs
@@ -7,7 +7,17 @@ public class HomeContentService
     {
         return new HomeViewModel
         {
-            WelcomeMessage = "Since 1999, we have continued to offer innovative services and products to the biomedical research community to advance knowledge of cellular signalling systems to facilitate development of improved disease diagnostics and therapeutics. Over 3,500 clients in academia and industry in over 40 countries have already utilized our services. We invite you to explore our website and learn the many innovative ways that we can assist you in your research endeavours. We strive to be one of your preferred commercial partners for systems proteomics research."
+            WelcomeMessage = "Since 1999, we have continued to offer innovative services and products " +
+            "to the biomedical research community to advance knowledge of cellular signalling systems " +
+            "to facilitate development of improved disease diagnostics and therapeutics. Over 3,500 " +
+            "clients in academia and industry in over 40 countries have already utilized our services. " +
+            "We invite you to explore our website and learn the many innovative ways that we can assist " +
+            "you in your research endeavours. We strive to be one of your preferred commercial partners " +
+            "for systems proteomics research.",
+
+            SignetMessage = "Kinexus is pleased to develop and maintain these open-access knowledgebases " +
+            "to foster the study of cell signalling systems and advance biomedical research in academia and " +
+            "industry for diagnostic and therapeutic solutions to confront human diseases."
         };
     }
 }
