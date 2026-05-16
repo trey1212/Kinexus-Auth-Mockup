f0afde80f3da2198212ada438fd5761754065404
trey1212 <rtarrayo2@my.bcit.ca>
2026-05-16 14:54:04 -0700

    Created SigNET page

--- Controllers/KnowledgebankController.cs
+++ Controllers/KnowledgebankController.cs
@@ -16,4 +16,5 @@ public class KnowledgebankController : Controller
     public IActionResult DrugProNet() => View();
     public IActionResult KinetAM() => View();
     public IActionResult Kinector() => View();
+    public IActionResult Signet() => View();
 }
