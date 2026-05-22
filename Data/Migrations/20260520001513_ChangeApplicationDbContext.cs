f6a629497e801fd4805974a61f1ca0dea0ddbe64
trey1212 <rtarrayo2@my.bcit.ca>
2026-05-21 23:35:49 -0700

    Reverted repo back to version before deploying

---
+++ Data/Migrations/20260520001513_ChangeApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KinexusMockup.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeApplicationDbContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                type: "TEXT",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 256);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                type: "TEXT",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 256,
+                oldNullable: true);
+        }
+    }
+}
--- phosphonet/README.md
+++
@@ -1,2 +0,0 @@
-# Kinexus Authentication Mockup
-This is a mockup of how the registration and login pages will look and function.
