7aa42b14f616c07587b286071c877b696d4cc75e
trey1212 <rtarrayo2@my.bcit.ca>
2026-05-21 18:34:37 -0700

    Added missing logic in Program.cs

--- kinexus/Program.cs
+++ kinexus/Program.cs
@@ -58,6 +58,22 @@ builder.Services.AddTransient<IEmailService, SmtpEmailService>();
 
 var app = builder.Build();
 
+// Connect to the migration files
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
